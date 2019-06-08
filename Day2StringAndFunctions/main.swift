3ee8c491e42c6e03daa55d59d76a1663f524d1bc
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-06-07 20:04:00 -0400

    Initial Commit

---
+++ Day2StringAndFunctions/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day2StringAndFunctions
+//
+//  Created by Neeraj Prasher on 2019-06-07.
+//  Copyright © 2019 lambton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
